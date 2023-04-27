f09c0c906fd7763f004c901e3cab9a9f30474ce8
RMXJ9 <77231097+RMXJ9@users.noreply.github.com>
2023-04-28 01:08:43 +0700

    Change address command master

--- MASTER/MASTER_SIMULATOR/MASTER_SIMULATION.ino
+++ MASTER/MASTER_SIMULATOR/MASTER_SIMULATION.ino
@@ -8,34 +8,34 @@
 #include <Wire.h>
 
 //SLAVE REGISTER ADDRESS MAP
-#define TGPS_ID           0x22  // KNACKSAT_II - TGPS ,ID ADDRESS
-#define TGPS_SCR          0xE0  // KNACKSAT_II - TGPS ,STATUS COMMAND RADFET ADDRESS 
-#define TGPS_SCC          0xE1  // KNACKSAT_II - TGPS ,STATUS COMMAND SD CARD ADDRESS 
-#define TGPS_CMD          0xF1  // KNACKSAT_II - TGPS ,MANAGEMENT
+#define TGPS_ID            0x22  // KNACKSAT_II - TGPS ,ID ADDRESS
+#define TGPS_SCR           0xE0  // KNACKSAT_II - TGPS ,STATUS COMMAND RADFET ADDRESS 
+#define TGPS_SCC           0xE1  // KNACKSAT_II - TGPS ,STATUS COMMAND SD CARD ADDRESS 
+#define TGPS_CMD           0xF5  // KNACKSAT_II - TGPS ,MANAGEMENT
 
 // SLAVE REGISTER VALUE
-#define TGPS_DATA_I        0xD0  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.1 ADDRESS
-#define TGPS_DATA_II       0xD1  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.2 ADDRESS
-#define TGPS_DATA_III      0xD2  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.3 ADDRESS
-#define TGPS_DATA_IV       0xD3  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.4 ADDRESS
-#define TGPS_DATA_V        0xD4  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.5 ADDRESS
-#define TGPS_DATA_VI       0xD5  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.6 ADDRESS
-#define TGPS_DATA_VII      0xD6  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.7 ADDRESS
-#define TGPS_DATA_VIII     0xD7  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.8 ADDRESS
+#define TGPS_DATA_I        0xD1  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.1 ADDRESS
+#define TGPS_DATA_II       0xD2  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.2 ADDRESS
+#define TGPS_DATA_III      0xD3  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.3 ADDRESS
+#define TGPS_DATA_IV       0xD4  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.4 ADDRESS
+#define TGPS_DATA_V        0xD5  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.5 ADDRESS
+#define TGPS_DATA_VI       0xD6  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.6 ADDRESS
+#define TGPS_DATA_VII      0xD7  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.7 ADDRESS
+#define TGPS_DATA_VIII     0xD8  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.8 ADDRESS
 
-#define TGPS_DATA_IX       0xD8  // KNACKSAT_II - TGPS ,****DATA   MICRO SD CARD ADDRESS
+#define TGPS_DATA_IX       0xD9  // KNACKSAT_II - TGPS ,****DATA   MICRO SD CARD ADDRESS
 
 //SLAVE REGISTER COMMAND
-#define TGPS_CMD_I         0xC0  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.1 ADDRESS
-#define TGPS_CMD_II        0xC1  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.2 ADDRESS
-#define TGPS_CMD_III       0xC2  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.3 ADDRESS
-#define TGPS_CMD_IV        0xC3  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.4 ADDRESS
-#define TGPS_CMD_V         0xC4  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.5 ADDRESS
-#define TGPS_CMD_VI        0xC5  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.6 ADDRESS
-#define TGPS_CMD_VII       0xC6  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.7 ADDRESS
-#define TGPS_CMD_VIII      0xC7  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.8 ADDRESS
+#define TGPS_CMD_I         0xC1  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.1 ADDRESS
+#define TGPS_CMD_II        0xC2  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.2 ADDRESS
+#define TGPS_CMD_III       0xC3  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.3 ADDRESS
+#define TGPS_CMD_IV        0xC4  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.4 ADDRESS
+#define TGPS_CMD_V         0xC5  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.5 ADDRESS
+#define TGPS_CMD_VI        0xC6  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.6 ADDRESS
+#define TGPS_CMD_VII       0xC7  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.7 ADDRESS
+#define TGPS_CMD_VIII      0xC8  // KNACKSAT_II - TGPS ,****DATA     RADFET NO.8 ADDRESS
 
-#define TGPS_CMD_IX        0xC8  // KNACKSAT_II - TGPS ,****DATA   MICRO SD CARD ADDRESS
+#define TGPS_CMD_IX        0xC9  // KNACKSAT_II - TGPS ,****DATA   MICRO SD CARD ADDRESS
 
 
 
@@ -64,49 +64,49 @@ void loop()
     {
       switch (N)
       {
-        case 0 :
-          Serial.println("DOWNLOAD 0");
+        case 1 :
+          Serial.println("DOWNLOAD I");
           DOWNLOAD(TGPS_ID,    TGPS_DATA_I, data_input, 32  , 50);
           break;
-        case 1 :
-          Serial.println("DOWNLOAD 1");
+        case 2 :
+          Serial.println("DOWNLOAD II");
           DOWNLOAD(TGPS_ID,   TGPS_DATA_II, data_input, 32  , 50);
           break;
-        case 2 :
-          Serial.println("DOWNLOAD 2");
+        case 3 :
+          Serial.println("DOWNLOAD III");
           DOWNLOAD(TGPS_ID,  TGPS_DATA_III, data_input, 32  , 50);
           break;
-        case 3 :
-          Serial.println("DOWNLOAD 3");
+        case 4 :
+          Serial.println("DOWNLOAD IV");
           DOWNLOAD(TGPS_ID,   TGPS_DATA_IV, data_input, 32  , 50);
           break;
-        case 4 :
-          Serial.println("DOWNLOAD 4");
+        case 5 :
+          Serial.println("DOWNLOAD V");
           DOWNLOAD(TGPS_ID,    TGPS_DATA_V, data_input, 32  , 50);
           break;
-        case 5 :
-          Serial.println("DOWNLOAD 5");
+        case 6 :
+          Serial.println("DOWNLOAD VI");
           DOWNLOAD(TGPS_ID,   TGPS_DATA_VI, data_input, 32  , 50);
           break;
-        case 6 :
-          Serial.println("DOWNLOAD 6");
+        case 7 :
+          Serial.println("DOWNLOAD VII");
           DOWNLOAD(TGPS_ID,  TGPS_DATA_VII, data_input, 32  , 50);
           break;
-        case 7 :
-          Serial.println("DOWNLOAD 7");
+        case 8 :
+          Serial.println("DOWNLOAD VIII");
           DOWNLOAD(TGPS_ID, TGPS_DATA_VIII, data_input, 32  , 50);
           break;
-        case 8 :
-          Serial.println("DOWNLOAD 8");
+        case 9 :
+          Serial.println("DOWNLOAD IX");
           DOWNLOAD(TGPS_ID,   TGPS_DATA_IX, data_input, 32  , 50);
           break;
-        case 9 :
-          Serial.println("DOWNLOAD STATUS");
+        case 11 :
+          Serial.println("DOWNLOAD STATUS CMD RADFET");
           DOWNLOAD(TGPS_ID,        TGPS_SCR,  data_cmd ,  1  , 50);
           break;
-        case 10:
-          Serial.println("DOWNLOAD STATUS");
-          DOWNLOAD(TGPS_ID,        TGPS_SCR,  data_cmd ,  1  , 50);
+        case 12:
+          Serial.println("DOWNLOAD STATUS CMD SD CARD");
+          DOWNLOAD(TGPS_ID,        TGPS_SCC,  data_cmd ,  1  , 50);
           break;
       }
     }
@@ -114,49 +114,40 @@ void loop()
     {
       switch (N)
       {
-        case 0 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_I, HEX);
+        case 1 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_I, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,     TGPS_CMD_I, data_cmd );
           break;
-        case 1 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_II, HEX );
+        case 2 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_II, HEX );
           UPLOAD(TGPS_ID, TGPS_CMD ,    TGPS_CMD_II, data_cmd );
           break;
-        case 2 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_III, HEX);
+        case 3 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_III, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,   TGPS_CMD_III, data_cmd );
           break;
-        case 3 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_IV, HEX);
+        case 4 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_IV, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,    TGPS_CMD_IV, data_cmd );
           break;
-        case 4 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_V, HEX);
+        case 5 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_V, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,     TGPS_CMD_V, data_cmd );
           break;
-        case 5 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_VI, HEX);
+        case 6 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_VI, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,    TGPS_CMD_VI, data_cmd );
           break;
-        case 6 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_VII, HEX);
+        case 7 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_VII, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,   TGPS_CMD_VII, data_cmd );
           break;
-        case 7 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_VIII, HEX);
+        case 8 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_VIII, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,  TGPS_CMD_VIII, data_cmd );
           break;
-        case 8 :
-          Serial.print("UPLOAD : ");
-          Serial.println(TGPS_CMD_IX, HEX);
+        case 9 :
+          Serial.print("UPLOAD : ");Serial.println(TGPS_CMD_IX, HEX);
           UPLOAD(TGPS_ID, TGPS_CMD ,    TGPS_CMD_IX, data_cmd );
           break;
       }
